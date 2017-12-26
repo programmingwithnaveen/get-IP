405754b22903029a306eed849f9d9b1af6a3dcf9
Kn <naveen.kn@accenture.com>
2017-12-26 00:54:20 -0800

    included code to retreive public IP

--- README.md
+++ README.md
@@ -5,11 +5,13 @@
 
 This project shows how to get IP address into Angular project using Javascript
 
-
-
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Documentation
+https://programmingwithnaveen.com/2017/12/26/get-client-ip-in-angular/
+ 
 
 ## WebRTC Reference
 1. https://github.com/diafygi/webrtc-ips
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare let ClientIP: any;
 
@@ -8,12 +9,22 @@ declare let ClientIP: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message = 'YOUR IP ADDRESS - ';
-  IP = '';
+   privateIP ;
+    publicIP;
+ 
 
-  constructor() {
-    this.IP = ClientIP;
+  constructor(private http: HttpClient) {
+    /**
+     * To read private IP
+     */
+    this.privateIP = ClientIP;
 
+    /**
+     * To read public IP
+     */
+    this.http.get('https://api.ipify.org?format=json').subscribe(data => {
+      this.publicIP=data['ip'];
+    });
   }
 
 }
